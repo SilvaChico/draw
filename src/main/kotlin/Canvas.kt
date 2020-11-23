017c1f1773d0218646f01ea4d468f1617a3e6ed8
Francisco Silva <franciscocrosilva@gmail.com>
2020-11-23 17:37:42 +0000

    Add canvas

---
+++ src/main/kotlin/Canvas.kt
@@ -0,0 +1,81 @@
+package draw
+
+ class Canvas(val width: Int, val height: Int) {
+
+    private val maxX = width
+    private val maxY = height
+
+    private var points = mutableMapOf<Position, String>()
+
+    init {
+        validateInputs(width, height)
+    }
+
+    fun drawLine(x1: Int, y1: Int, x2: Int, y2: Int) {
+
+        val firstPosition = Position(x1, y1)
+        val secondPosition = Position(x2, y2)
+
+        if (! isValid(firstPosition) || ! isValid(secondPosition))
+            throw Exception("Line should not overflow the canvas")
+
+        try {
+            Line(firstPosition, secondPosition).draw().forEach { position ->
+                points.put(position, "X")
+            }
+        } catch (e: Exception) {
+        }
+    }
+
+    fun drawRectangle(x1: Int, y1: Int, x2: Int, y2: Int) {
+
+        val upperLeftCorner = Position(x1, y1)
+        val lowerRightCorner = Position(x2, y2)
+
+        if (! isValid(upperLeftCorner) || ! isValid(lowerRightCorner))
+            throw Exception("Rectangle should not overflow the canvas")
+
+        try {
+            Rectangle(upperLeftCorner, lowerRightCorner).draw().forEach { position ->
+                points.put(position, "X")
+            }
+        } catch (e: Exception) {
+            println(e.message)
+        }
+    }
+
+    fun render() {
+        val rows: IntRange = (0..height + 1)
+        val columns: IntRange = (0..width + 1)
+        var buffer: String
+
+        for (y in rows) {
+            for (x in columns) {
+                if (isFirstOrLastRow(y)) buffer = "-"
+                else if (isFirstOrLastColumn(x)) buffer = "|"
+                else buffer = points[Position(x, y)] ?: " "
+                print(buffer)
+            }
+            print("\n")
+        }
+    }
+
+    private fun isFirstOrLastRow(y: Int): Boolean {
+        return y == 0 || y == height + 1
+    }
+
+    private fun isFirstOrLastColumn(x: Int): Boolean {
+        return x == 0 || x == width + 1
+    }
+
+    private fun isValid(position: Position): Boolean {
+        return position.x <= maxX && position.y <= maxY && position.x > 0 && position.y > 0
+    }
+
+     private fun validateInputs(width: Int, height: Int) {
+        if (width <= 0)
+            throw Exception("Width must be greater than zero")
+        if (height <= 0)
+            throw Exception("Height must be greater than zero")
+    }
+ }
---
+++ src/test/kotlin/CanvasTest.kt
@@ -0,0 +1,64 @@
+package draw
+
+import io.mockk.*
+import kotlin.test.*
+
+ class CanvasTest {
+    @Test
+    fun `creates a canvas`() {
+        val canvas = Canvas(20, 4)
+        assertTrue { canvas.width == 20 }
+        assertTrue { canvas.height == 4 }
+    }
+
+     @Test
+    fun `creates a canvas with negative value`() {
+        try {
+            val canvas = Canvas(20, -4)
+            canvas.render()
+            assertTrue { false }
+        } catch (e: Exception) {
+            assertTrue { e.message == "height must be greater than zero" }
+        }
+    }
+
+     @Test
+    fun `draws a line`() {
+        val canvas = Canvas(20, 4)
+        canvas.drawLine(1, 2, 6, 2)
+        canvas.render()
+        assertTrue { canvas.width == 20 }
+        assertTrue { canvas.height == 4 }
+    }
+
+     @Test
+    fun `draws a rectangle`() {
+        val canvas = Canvas(20, 4)
+        canvas.drawRectangle(16, 1, 20, 3)
+        canvas.render()
+        assertTrue { canvas.width == 20 }
+        assertTrue { canvas.height == 4 }
+    }
+
+    @Test
+    fun `draws a line overflowing the canvas`() {
+        val canvas = Canvas(20, 4)
+        try {
+            canvas.drawLine(1, 2, 21, 2)
+            assertTrue { false }
+        } catch (e: Exception) {
+            assertTrue { e.message == "Line should not overflow the canvas" }
+        }
+    }
+
+        @Test
+    fun `draws a rectangle overflowing the canvas`() {
+        val canvas = Canvas(20, 4)
+        try {
+            canvas.drawRectangle(16, 1, 21, 3)
+            assertTrue { false }
+        } catch (e: Exception) {
+            assertTrue { e.message == "Rectangle should not overflow the canvas" }
+        }
+    }
+ }
