badd4b9474219eb054da12e98787fb945822e75d
Francisco Silva <franciscocrosilva@gmail.com>
2020-11-23 17:50:57 +0000

    Add canvas creation to app

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,26 +1,54 @@
-package main.app
+package draw
+
+fun main() {
+    App().start()
+}
 
 class App {
 
-    private var canvas = null;
+    private var canvas: Canvas? = null
 
-    fun handleUserInput() {
+    fun start() {
         while (true) {
             print("enter command:")
             val command = readLine()
-            if (!command.isNullOrBlank())
+            if (!command.isNullOrEmpty())
                 when {
-                    command.startsWith("C") -> println("Not yet implemented")
+                    command.startsWith("C") -> createCanvas(command)
                     command.startsWith("L") -> println("Not yet implemented")
                     command.startsWith("R") -> println("Not yet implemented")
-                    command == "Q" -> return;
-                    else -> println("Command not valid, enter Q to quit")
+                    command == "Q" -> return
+                    else -> println("Command not valid, enter Q to quit or try again")
                 }
+            val localCanvas = canvas
+            if (localCanvas != null)
+                localCanvas.render()
+            }
         }
+
+        private fun createCanvas(command: String) {
+            try {
+                val width = command.split(" ")[1].toInt()
+                val height = command.split(" ")[2].toInt()
+                if (! isValidValue(width) || ! isValidValue(height))
+                    println("Input values are not valid")
+                else
+                    canvas = Canvas(width, height)
+                } catch (e: Exception) {
+                    println("Input values are not valid")
+                    println(e.message)
+                }
+            }
+
+        // private fun isValidCommand(command: String?): Boolean {
+            //     if (command.isNullOrBlank()) return false
+    //     if (command.startsWith("C") || 
+    //     ((command.startsWith("L") || command.startsWith("R")) && !canvas.isNullOrBlank())
+    //         return true
+    //     return false
+    // }
+
+    private fun isValidValue(value: Int): Boolean {
+        return value >= 0
     }
-
-}
-
-fun main() {
-    App().handleUserInput()
 }
--- src/test/kotlin/AppTest.kt
+++ src/test/kotlin/AppTest.kt
@@ -1,13 +1,31 @@
-package main.app
+package draw
 import io.mockk.*
 import kotlin.test.*
 
 class AppTest {
     @Test fun `handle user input Q to quit`() {
-        val classUnderTest = App();
+        val classUnderTest = App()
         mockkStatic("kotlin.io.ConsoleKt")
         every { readLine() } returns "Q"
-        classUnderTest.handleUserInput()
+        classUnderTest.start()
+        val shouldQuitProgram = true
+        assertTrue { shouldQuitProgram }
+    }
+
+    @Test fun `creates canvas`() {
+        val classUnderTest = App()
+        mockkStatic("kotlin.io.ConsoleKt")
+        every { readLine() } returns "C 20 4" andThen "Q"
+        classUnderTest.start()
+        val shouldQuitProgram = true
+        assertTrue { shouldQuitProgram }
+    }
+
+    @Test fun `creates canvas with an invalid value`() {
+        val classUnderTest = App()
+        mockkStatic("kotlin.io.ConsoleKt")
+        every { readLine() } returns "C 20 $" andThen "Q"
+        classUnderTest.start()
         val shouldQuitProgram = true
         assertTrue { shouldQuitProgram }
     }
